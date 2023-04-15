d364759690a98bb895654c2f8bee81f33ee3e5d6
Hill <125409221+NARI1108@users.noreply.github.com>
2023-04-15 15:51:39 +0430

    Connecting the Views layer Xml to double_Player.java

--- app/src/main/java/com/example/tictactoe/double_Player.java
+++ app/src/main/java/com/example/tictactoe/double_Player.java
@@ -8,22 +8,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.Arrays;
+
 
 public class double_Player extends AppCompatActivity {
-    final static int NULL=0;
-    final static int PLAYER_1=1;
-    final static int PLAYER_2=2;
-    final static int NO_WINNER=3;
-    int turn = 1;
-    int [] status = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,};
-
-    int winner;
-    int [][] winner_position = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
-    TextView txt_player1, txt_player2, txt_score1, txt_score2;
+    final static int NULL = 0;
+    final static int PLAYER_1 = 1;
+    final static int PLAYER_2 = 2;
+    final static int NO_WINNER = 3;
+    int turn = PLAYER_1 , number = -1;
+    int[] status = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+    int winner = number;
+    int[][] winner_position = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+    TextView txt_player1, txt_player2, txt_score1, txt_score2, txt_result;
     ImageView img_0, img_1, img_2, img_3, img_4, img_5, img_6, img_7, img_8;
     String player_name1, player_name2;
+    Button btn_play_again;
+    RelativeLayout result_layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,16 +47,22 @@ public class double_Player extends AppCompatActivity {
         img_6 = findViewById(R.id.img_6);
         img_7 = findViewById(R.id.img_7);
         img_8 = findViewById(R.id.img_8);
-    playerNamesDialog();
+
+        txt_result = findViewById(R.id.txt_result);
+
+        btn_play_again = findViewById(R.id.btn_play_again);
+
+        result_layout = findViewById(R.id.result_layout);
+        playerNamesDialog();
     }
-    private void playerNamesDialog(){
-      Dialog  playerNameDialog = new Dialog(this);
-      playerNameDialog.setContentView(R.layout.player_name_dialog);
+    private void playerNamesDialog() {
+        Dialog playerNameDialog = new Dialog(this);
+        playerNameDialog.setContentView(R.layout.player_name_dialog);
 
         playerNameDialog.setCancelable(false);
 
-        EditText edt_player1 =playerNameDialog.findViewById(R.id.edt_player1);
-        EditText edt_player2 =playerNameDialog.findViewById(R.id.edt_player2);
+        EditText edt_player1 = playerNameDialog.findViewById(R.id.edt_player1);
+        EditText edt_player2 = playerNameDialog.findViewById(R.id.edt_player2);
         Button btn_play = playerNameDialog.findViewById(R.id.btn_play);
 
         btn_play.setOnClickListener(new View.OnClickListener() {
@@ -61,8 +72,8 @@ public class double_Player extends AppCompatActivity {
                 player_name1 = edt_player1.getText().toString().trim();
                 player_name2 = edt_player2.getText().toString().trim();
 
-                if(player_name1.equals("")) player_name1="Player1";
-                if(player_name2.equals("")) player_name2="Player2";
+                if (player_name1.equals("")) player_name1 = "Player1";
+                if (player_name2.equals("")) player_name2 = "Player2";
 
                 txt_player1.setText(player_name1);
                 txt_player2.setText(player_name2);
@@ -76,29 +87,26 @@ public class double_Player extends AppCompatActivity {
 
         int tag = Integer.parseInt((String) view.getTag());
 
-        if (status[tag] != 0) return;
+            if (status[tag] != NULL ) return;
 
-        ImageView imageView = (ImageView) view;
-        if (turn == PLAYER_1) {
-            imageView.setImageResource(R.drawable.multiply);
-            turn = PLAYER_2;
-            status[tag] = PLAYER_1;
-        } else {
-            imageView.setImageResource(R.drawable.circle);
+            ImageView imageView = (ImageView) view;
+            if (turn == PLAYER_1) {
+                imageView.setImageResource(R.drawable.multiply);
+                turn = PLAYER_2;
+                status[tag] = PLAYER_1;
+            } else {
+                imageView.setImageResource(R.drawable.circle);
 
-            turn = PLAYER_1;
-            status[tag] = PLAYER_2;
-        }
-        winner = checkWinner();
-        Toast.makeText(this, winner+"", Toast.LENGTH_SHORT).show();
+                turn = PLAYER_1;
+                status[tag] = PLAYER_2;
+            }
     }
-    private int checkWinner(){
-        for(int [] win_pos : winner_position){
-            if(status[win_pos[0]] == status[win_pos[1]] && status[win_pos[1]] == status[win_pos[2]] && status[win_pos[0]] != NULL){
+    private int checkWinner() {
+        for (int[] win_pos : winner_position) {
+            if (status[win_pos[0]] == status[win_pos[1]] && status[win_pos[1]] == status[win_pos[2]] && status[win_pos[0]] != NULL) {
                 return status[win_pos[0]];
             }
         }
         return NO_WINNER;
     }
-
 }
