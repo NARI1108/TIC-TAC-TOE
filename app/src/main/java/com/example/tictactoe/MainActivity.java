df734aa17b5ca7f79faa138777f94eb53278c4f9
Hill <125409221+NARI1108@users.noreply.github.com>
2023-04-19 13:06:46 +0430

    Adding two activities to play on hard and easy levels

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
