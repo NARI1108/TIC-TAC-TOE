f9372e7f34d4824f56f47f01c4627b0760fd1a78
Hill <125409221+NARI1108@users.noreply.github.com>
2023-04-24 20:21:51 +0430

    Creating a 2D array for the robot to find its position on the table and fill the houses with the intention of winning, not randomly.

--- app/src/main/java/com/example/tictactoe/BaseActivity.java
+++ app/src/main/java/com/example/tictactoe/BaseActivity.java
@@ -23,6 +23,7 @@ public class BaseActivity extends AppCompatActivity {
     int turn = PLAYER_1 , number = -1, score_1=0 ,score_2=0 ;
     int[] status = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
     int winner = number;
+    int [][] robot_action_2_position={{0,1},{1,0},{1,2},{2,1},{3,4},{4,3},{4,5},{5,4},{6,7},{7,6},{7,8},{8,7},{0,3},{3,0},{3,6},{6,3},{1,4},{4,1},{4,7},{7,4},{2,5},{5,2},{5,8},{8,5},{0,4},{4,0},{4,8},{2,4},{4,2},{4,6},{6,4}};
     int[][] winner_position = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
     int [] final_winner_position;
     ArrayList<ImageView> imageView_List = new ArrayList<>();
