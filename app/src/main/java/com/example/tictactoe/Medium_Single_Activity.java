7641e9bd22a46318768ef195dce490bda8e69050
Hill <125409221+NARI1108@users.noreply.github.com>
2023-04-26 13:04:22 +0430

    The method of clicking on the houses by the user

--- app/src/main/java/com/example/tictactoe/Medium_Single_Activity.java
+++ app/src/main/java/com/example/tictactoe/Medium_Single_Activity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Medium_Single_Activity extends BaseActivity {
 
@@ -83,5 +84,23 @@ public class Medium_Single_Activity extends BaseActivity {
         if(click_snd!=null) click_snd.release();
         if(winner_snd!=null) winner_snd.release();
         super.onPause();
+    }
+    public void imagesClick(View view) {
+
+        int tag = Integer.parseInt((String) view.getTag());
+
+        if (status[tag] != NULL || game_over) return;
+
+        ImageView imageView = (ImageView) view;
+
+
+        imageView.setImageResource(R.drawable.circle);
+
+        turn = PLAYER_1;
+        status[tag] = PLAYER_2;
+
+        click_snd.start();
+        getResult();
+        robotAction_2();
     }
 }
