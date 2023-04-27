5afc02421948b000842666d812fe54240bd31fcc
Hill <125409221+NARI1108@users.noreply.github.com>
2023-04-27 12:59:54 +0430

    Deleting the method od setContentVIew() because of we don't need to activity_base.XML

--- app/src/main/java/com/example/tictactoe/BaseActivity.java
+++ app/src/main/java/com/example/tictactoe/BaseActivity.java
@@ -25,7 +25,7 @@ public class BaseActivity extends AppCompatActivity {
     int winner = number;
     int [][] robot_action_2_position={{0,1},{1,0},{1,2},{2,1},{3,4},{4,3},{4,5},{5,4},{6,7},{7,6},{7,8},{8,7},{0,3},{3,0},{3,6},{6,3}
             ,{1,4},{4,1},{4,7},{7,4},{2,5},{5,2},{5,8},{8,5},{0,4},{4,0},{4,8},{2,4},{4,2},{4,6},{6,4}};
-    int robot_action_3_position [][] = {{0,1,2},{1,2,0},{0,2,1},{3,4,5},{4,5,3},{3,5,4},{6,7,8},{7,8,6},{6,8,7},{0,3,6},{3,6,0},{0,6,3}
+    int [][] robot_action_3_position  = {{0,1,2},{1,2,0},{0,2,1},{3,4,5},{4,5,3},{3,5,4},{6,7,8},{7,8,6},{6,8,7},{0,3,6},{3,6,0},{0,6,3}
             ,{1,4,7},{4,7,1},{1,7,4},{2,5,8},{5,8,2},{2,8,5},{0,4,8},{4,8,0},{0,8,4},{2,4,6},{4,6,2},{2,6,4}};
     int[][] winner_position = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
     int [] final_winner_position;
@@ -39,7 +39,6 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
     }
     public int getRandomNumber(int max){
         Random random = new Random();
