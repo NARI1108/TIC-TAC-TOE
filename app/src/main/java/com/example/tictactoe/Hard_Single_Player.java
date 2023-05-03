e895b79171543d91210f9f4a28a5f45e9f60bfb1
Hill <125409221+NARI1108@users.noreply.github.com>
2023-05-03 12:58:31 +0430

    Deleting the onClick=resetGame in a double_Player activity. and putting some Boolean in the reset game method called robot. Adding the method resetGame for the game start button in easy, medium and hard activities and double_Player.java. It is present in the activities of the robot. The value of the reset game method is equal to true and otherwise equal to false.

--- app/src/main/java/com/example/tictactoe/Hard_Single_Player.java
+++ app/src/main/java/com/example/tictactoe/Hard_Single_Player.java
@@ -40,6 +40,12 @@ public class Hard_Single_Player extends BaseActivity {
         result_layout = findViewById(R.id.result_layout);
 
         playerNamesDialog();
+        btn_play_again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetGame(false);
+            }
+        });
     }
     private void playerNamesDialog() {
         Dialog playerNameDialog = new Dialog(this);
